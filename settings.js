25d7cca909bc6ca1516da316de4e80d25bf89ab4
Neemias Renan <neemiasrenan2015@gmail.com>
2023-10-04 15:18:51 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,9 @@
 var CLIENT_ID = 'o1apWATIpyxXWVdA5SbTrb5rS8WRuRFsjZ3CwGQW';
-var REDIRECT_URI = 'https://neemias-renan.github.io/qrc_refeitoriocnat/';
+var REDIRECT_URI = 'www2.ifrn.edu.br/refeitoriocnat/';
 var SUAP_URL = 'https://suap.ifrn.edu.br';
 var SCOPE = 'matricula';
 
 // https://neemias-renan.github.io/qrc_refeitoriocnat/
 // http://localhost:8000/index.html
+// www2.ifrn.edu.br/refeitoriocnat/
 
